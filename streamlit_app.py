597b210c7164d41aee3399fc580d8ebe6037d96a
Ellie <125852819+EL1214@users.noreply.github.com>
2023-06-01 21:51:00 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+num1  = st.number_input('Enter num1')
+num2 = st.number_input('Enter num2')
+st.write('The numbers are', num1 + num2)
+
+symbol = st.radio("Click one of the mathematical calculation symbols",
+                  ('Addition', 'Subtraction', 'Multiplying', 'Division'))
+if symbol == 'Addition':
+    st.write('The answer is', num1 + num2)
+    
+elif symbol == 'Subtraction':
+    st.write('The answer is', num1 - num2)
+    
+elif symbol == 'Multiplying':
+    st.write('The answer is', num1 * num2)
+    
+else:
+    st.write('The answer is', num1 / num2)
